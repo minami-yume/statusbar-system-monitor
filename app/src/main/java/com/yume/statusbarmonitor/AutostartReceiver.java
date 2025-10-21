a334fbe3ad9d88af9adeeffd01d88f2179893d29
minamiyume <minami_yume@outlook.com>
2025-10-21 19:46:24 +0800

    Added Autostart.

--- app/src/main/java/com/yume/statusbarmonitor/AutostartReceiver.java
+++ app/src/main/java/com/yume/statusbarmonitor/AutostartReceiver.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
