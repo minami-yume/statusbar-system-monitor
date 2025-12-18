2c701b298ceef667c5062120b9456822a4566c13
minamiyume <minami_yume@outlook.com>
2025-12-18 20:21:22 +0800

    feat: implement flowing dash animation for progress ring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "2.0.0"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
